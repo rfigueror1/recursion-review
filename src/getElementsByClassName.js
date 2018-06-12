3fd704e6914c6b90ab9c20cc90e3c31465ac8b4c
rfigueror1 <rfigueror@gmail.com>
2018-06-11 20:25:47 -0700

    added pseudocdoe

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,23 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var completeBody = document.body.childNodes;
+  var result = [];
+  for(var i = 0; i<completeBody.length; i++){
+    if(completeBody[i].childNodes.length >= 1){
+      getElementsByClassName(completeBody[i]);
+    }else{
+      if(completeBody[i].className === className){
+        result.push(completeBody[i]);
+      }
+    }
+  }
+  console.log(result);
+  return result;  // your code here
 };
+//input is className and the DOM
+//We want to get initially a list of all the nodes and to compare each name with 
+//output = array of nodes
+//document.body to get complete body of DOM
+
